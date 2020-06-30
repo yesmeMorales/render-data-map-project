10100a57e7e3e0b009d7aa6ff4ca91abf98f9782
Yesme <moralesyesme@gmail.com>
2020-06-30 14:42:13 -0500

    Adding the ScatterplotLayer

--- main.js
+++ main.js
@@ -2,18 +2,42 @@ const $map = document.getElementById("map");
 const $controls = document.getElementById("controls");
 const EUROPE_CENTER = { lat: 47.582798, lng: 9.707756 };
 
-const { ScatterplotLayer, GoogleMapsOverlay } = deck;
+const { DeckGl, ScatterplotLayer, GoogleMapsOverlay } = deck;
 
 let GMAP, DECKGL_OVERLAY;
 
 // function buttonTemplate(country) {
 //   return `
-
 //   `;
 // }
 
-function updateMap(country) {
+async function updateMap() {
+  const $activeElement = document.querySelector(".is-active");
+  if ($activeElement) {
+    $activeElement.classList.remove("is-active");
+  }
+  this.classList.add("is-active");
+  let country = this.textContent.replace(/ /g, "").toLowerCase();
+  let layer = await getLayer(country);
+  DECKGL_OVERLAY.setProps({ layers: [layer] });
   console.log(this.textContent);
+}
+
+async function getLayer(country = "austria") {
+  const request_country = await fetch(`./data/${country}.json`);
+  const data_country = await request_country.json();
+  return await new ScatterplotLayer({
+    id: "trees",
+    data: data_country,
+    radiusMinPixels: 1,
+    radiusMaxPixels: 100,
+    lineWidthMinPixels: 1,
+    //PD: is first lng and then lat
+    getPosition: (d) => [d.lng, d.lat],
+    getRadius: (d) => 50,
+    getFillColor: (d) => [0, 155, 115],
+    getLineColor: (d) => [30, 30, 30],
+  });
 }
 
 function renderButtonElement(country) {
@@ -30,11 +54,17 @@ async function renderButtons() {
   console.log(countries);
 }
 
-function initMap() {
+async function initMap() {
   GMAP = new google.maps.Map($map, {
     zoom: 4,
     center: EUROPE_CENTER,
   });
+  //inicializamos deckGl
+  DECKGL_OVERLAY = new GoogleMapsOverlay();
+  // Generamos una capa
+  DECKGL_OVERLAY.setMap(GMAP);
+  //las capas son un array
+  DECKGL_OVERLAY.setProps({ layers: [await getLayer()] });
   renderButtons();
 }
 
